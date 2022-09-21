f8dd25473354eaf379208d2203f141708a5d679d
juan pablo <jpagudelo2000@gmal.com>
2022-09-20 21:05:13 -0500

    animations background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'weather';
+  isSun: Boolean = false;
+  chageSun() {
+    this.isSun = !this.isSun;
+    document.body.style.background = this.isSun ? '#B28920' : '#121C41';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardWeatherComponent } from './components/card-weather/card-weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardWeatherComponent
   ],
   imports: [
     BrowserModule
